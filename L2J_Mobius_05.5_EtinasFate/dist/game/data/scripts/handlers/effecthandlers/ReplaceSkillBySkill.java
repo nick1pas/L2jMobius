ba09d61dc4cab452924d6e8a3b1c0940fcee9161
MobiusDevelopment <8391001+MobiusDevelopment@users.noreply.github.com>
2022-10-15 21:40:37 +0000

    Addition of ReplaceSkillBySkill canStart transformation condition. Thanks to Liamxroy.

--- L2J_Mobius_05.5_EtinasFate/dist/game/data/scripts/handlers/effecthandlers/ReplaceSkillBySkill.java
+++ L2J_Mobius_05.5_EtinasFate/dist/game/data/scripts/handlers/effecthandlers/ReplaceSkillBySkill.java
@@ -45,38 +45,41 @@ public class ReplaceSkillBySkill extends AbstractEffect
 	}
 	
 	@Override
+	public boolean canStart(Creature effector, Creature effected, Skill skill)
+	{
+		return effected.isPlayer() && !effected.isTransformed();
+	}
+	
+	@Override
 	public void onStart(Creature effector, Creature effected, Skill skill, Item item)
 	{
-		if (effected.isPlayer())
+		final Player player = effected.getActingPlayer();
+		final Skill knownSkill = player.getKnownSkill(_existingSkill.getSkillId());
+		if ((knownSkill == null) || (knownSkill.getLevel() < _existingSkill.getSkillLevel()))
 		{
-			final Player player = effected.getActingPlayer();
-			final Skill knownSkill = player.getKnownSkill(_existingSkill.getSkillId());
-			if ((knownSkill == null) || (knownSkill.getLevel() < _existingSkill.getSkillLevel()))
-			{
-				return;
-			}
-			
-			final Skill addedSkill = SkillData.getInstance().getSkill(_replacementSkill.getSkillId(), _replacementSkill.getSkillLevel() < 1 ? knownSkill.getLevel() : _replacementSkill.getSkillLevel(), knownSkill.getSubLevel());
-			player.addSkill(addedSkill, false);
-			player.addReplacedSkill(_existingSkill.getSkillId());
-			for (Shortcut shortcut : player.getAllShortCuts())
-			{
-				if ((shortcut.getType() == ShortcutType.SKILL) && (shortcut.getId() == knownSkill.getId()) && (shortcut.getLevel() == knownSkill.getLevel()))
-				{
-					final int slot = shortcut.getSlot();
-					final int page = shortcut.getPage();
-					final int characterType = shortcut.getCharacterType();
-					player.deleteShortCut(slot, page);
-					final Shortcut newShortcut = new Shortcut(slot, page, ShortcutType.SKILL, addedSkill.getId(), addedSkill.getLevel(), addedSkill.getSubLevel(), characterType);
-					player.registerShortCut(newShortcut);
-					player.sendPacket(new ShortCutRegister(newShortcut));
-				}
-			}
-			
-			player.removeSkill(knownSkill, false);
-			player.sendSkillList();
-			ThreadPool.schedule(() -> player.sendPacket(new ShortCutInit(player)), 1100);
+			return;
 		}
+		
+		final Skill addedSkill = SkillData.getInstance().getSkill(_replacementSkill.getSkillId(), _replacementSkill.getSkillLevel() < 1 ? knownSkill.getLevel() : _replacementSkill.getSkillLevel(), knownSkill.getSubLevel());
+		player.addSkill(addedSkill, false);
+		player.addReplacedSkill(_existingSkill.getSkillId());
+		for (Shortcut shortcut : player.getAllShortCuts())
+		{
+			if ((shortcut.getType() == ShortcutType.SKILL) && (shortcut.getId() == knownSkill.getId()) && (shortcut.getLevel() == knownSkill.getLevel()))
+			{
+				final int slot = shortcut.getSlot();
+				final int page = shortcut.getPage();
+				final int characterType = shortcut.getCharacterType();
+				player.deleteShortCut(slot, page);
+				final Shortcut newShortcut = new Shortcut(slot, page, ShortcutType.SKILL, addedSkill.getId(), addedSkill.getLevel(), addedSkill.getSubLevel(), characterType);
+				player.registerShortCut(newShortcut);
+				player.sendPacket(new ShortCutRegister(newShortcut));
+			}
+		}
+		
+		player.removeSkill(knownSkill, false);
+		player.sendSkillList();
+		ThreadPool.schedule(() -> player.sendPacket(new ShortCutInit(player)), 1100);
 	}
 	
 	@Override
