9343132e4b1ba35449ca971d9cb96b3592800bf6
MobiusDevelopment <8391001+MobiusDevelopment@users.noreply.github.com>
2022-12-13 07:07:52 +0000

    Multi enchant RequestEnchantItem improvements. Contributed by Fakee.

--- L2J_Mobius_10.2_MasterClass/java/org/l2jmobius/gameserver/network/clientpackets/enchant/multi/ExRequestStartMultiEnchantScroll.java
+++ L2J_Mobius_10.2_MasterClass/java/org/l2jmobius/gameserver/network/clientpackets/enchant/multi/ExRequestStartMultiEnchantScroll.java
@@ -56,7 +56,7 @@ public class ExRequestStartMultiEnchantScroll implements ClientPacket
 		
 		final Item scroll = player.getInventory().getItemByObjectId(_scrollObjectId);
 		final EnchantScroll scrollTemplate = EnchantItemData.getInstance().getEnchantScroll(scroll);
-		if (scrollTemplate == null)
+		if (scrollTemplate == null || scrollTemplate.isBlessed() || scrollTemplate.isBlessedDown() || scrollTemplate.isSafe() || scrollTemplate.isGiant())
 		{
 			player.sendPacket(new ExResetSelectMultiEnchantScroll(player, _scrollObjectId, 1));
 			return;
