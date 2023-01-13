08c99f885a216e0319bca818e20b3f257a8dd661
MobiusDevelopment <8391001+MobiusDevelopment@users.noreply.github.com>
2023-01-13 03:51:26 +0000

    Heretics Catacomb location adjustments. Contributed by L2Hanesfor.

--- L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
+++ L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
@@ -1,2 +1,5 @@
 -- Remove Schuttgart teleports.
 DELETE FROM `teleport` WHERE `Description` like '%Schuttgart%';
+
+-- Heretics Catacomb.
+UPDATE teleport SET loc_x = 43375, loc_y = 143937, loc_z = -5380 WHERE Description = "Cat Heretics Entrance";
