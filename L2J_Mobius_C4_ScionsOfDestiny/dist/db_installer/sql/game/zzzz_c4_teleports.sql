2e365a7063c5ce40b446c334884aae592431f990
MobiusDevelopment <mobius@cyber-wizard.com>
2023-02-14 16:21:32 +0200

    Updated Catacomb of the Forbidden Path spawns and teleport. Contributed by L2Hanesfor.

--- L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
+++ L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
@@ -4,5 +4,8 @@ DELETE FROM `teleport` WHERE `Description` like '%Schuttgart%';
 -- Heretics Catacomb.
 UPDATE teleport SET loc_x = 43375, loc_y = 143937, loc_z = -5380 WHERE Description = "Cat Heretics Entrance";
 
--- Apostate Catacomb 
+-- Apostate Catacomb.
 UPDATE teleport SET loc_x = 78055, loc_y = 78405, loc_z = -5128 WHERE Description = "Cat Apostate Entrance";
+
+-- Catacomb of the Forbidden Path.
+UPDATE teleport SET loc_x = 114286, loc_y = 84543, loc_z = -6543 WHERE Description = "Cat ForbiddenPath Entrance";
