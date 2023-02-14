0ed5c82e19a12faf97354e37be6acb1441e46da1
MobiusDevelopment <mobius@cyber-wizard.com>
2023-02-14 16:14:57 +0200

    Updated Apostate Catacomb spawns and teleport. Contributed by L2Hanesfor.

--- L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
+++ L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/zzzz_c4_teleports.sql
@@ -3,3 +3,6 @@ DELETE FROM `teleport` WHERE `Description` like '%Schuttgart%';
 
 -- Heretics Catacomb.
 UPDATE teleport SET loc_x = 43375, loc_y = 143937, loc_z = -5380 WHERE Description = "Cat Heretics Entrance";
+
+-- Apostate Catacomb 
+UPDATE teleport SET loc_x = 78055, loc_y = 78405, loc_z = -5128 WHERE Description = "Cat Apostate Entrance";
