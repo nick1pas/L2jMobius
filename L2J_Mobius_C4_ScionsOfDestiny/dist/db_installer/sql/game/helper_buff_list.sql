c7a80468db1e0c8775dd34b4dddf4001efc8c245
MobiusDevelopment <mobius@cyber-wizard.com>
2023-05-14 12:29:15 +0300

    Adjusted helper_buff_list levels. Contributed by emilianoify.

--- L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/helper_buff_list.sql
+++ L2J_Mobius_C4_ScionsOfDestiny/dist/db_installer/sql/game/helper_buff_list.sql
@@ -18,17 +18,17 @@ PRIMARY KEY  (`id`)
 #----------------------------
 
 insert  into helper_buff_list values 
-(0, 4322, 'WindWalk', 1, 8, 39, 'false'),
-(1, 4323, 'Shield', 1, 11, 39, 'false'),
-(2, 4338, 'Life Cubic', 1, 16, 36, 'false'),
-(3, 4324, 'Bless the Body', 1, 12, 38, 'false'),
-(4, 4325, 'Vampiric Rage', 1, 13, 38, 'false'),
-(5, 4326, 'Regeneration', 1, 14, 38, 'false'),
-(6, 4327, 'Haste', 1, 15, 37, 'false'),
-(7, 4322, 'WindWalk', 1, 8, 39, 'true'),
-(8, 4323, 'Shield', 1, 11, 39, 'true'),
-(9, 4338, 'Life Cubic', 1, 16, 36, 'true'),
-(10, 4328, 'Bless the Soul', 1, 12, 38, 'true'),
-(11, 4329, 'Acumen', 1, 13, 38, 'true'),
-(12, 4330, 'Concentration', 1, 14, 38, 'true'),
-(13, 4331, 'Empower', 1, 15, 37, 'true');
+(0, 4322, 'WindWalk', 1, 8, 24, 'false'),
+(1, 4323, 'Shield', 1, 11, 24, 'false'), 
+(2, 4338, 'Life Cubic', 1, 16, 19, 'false'), 
+(3, 4324, 'Bless the Body', 1, 12, 23, 'false'), 
+(4, 4325, 'Vampiric Rage', 1, 13, 22, 'false'), 
+(5, 4326, 'Regeneration', 1, 14, 21, 'false'), 
+(6, 4327, 'Haste', 1, 15, 20, 'false'), 
+(7, 4322, 'WindWalk', 1, 8, 24, 'true'),
+(8, 4323, 'Shield', 1, 11, 24, 'true'), 
+(9, 4338, 'Life Cubic', 1, 16, 19, 'true'), 
+(10, 4328, 'Bless the Soul', 1, 12, 23, 'true'), 
+(11, 4329, 'Acumen', 1, 13, 22, 'true'), 
+(12, 4330, 'Concentration', 1, 14, 21, 'true'), 
+(13, 4331, 'Empower', 1, 15, 20, 'true');
